556cd83d575607918d112b28e5a28805e7605685
João <joaolages.dev@gmail.com>
2021-06-10 13:59:06 -0300

    Validação do pedido com Flunt

--- Store.Domain/Entities/Order.cs
+++ Store.Domain/Entities/Order.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using Flunt.Validations;
 using Store.Domain.Enums;
 
 namespace Store.Domain.Entities
 {
-    public class Order
+    public class Order : Entity
     {
         public Order(Customer customer, decimal deliveryFee, Discount discount)
         {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .IsNotNull(customer, "Customer", "Cliente inválido")
+            );
+
             Customer = customer;
             Date = DateTime.Now;
             Number = Guid.NewGuid().ToString().Substring(0, 8);
@@ -28,7 +35,9 @@ namespace Store.Domain.Entities
         public void AddItem(Product product, int quantity)
         {
             var item = new OrderItem(product, quantity);
-            Items.Add(item);
+            
+            if(item.Valid)
+                Items.Add(item);
         }
 
         public decimal Total()
@@ -56,5 +65,8 @@ namespace Store.Domain.Entities
             Status = EOrderStatus.Canceled;
         }
 
+        public void Complete(){
+            Status = EOrderStatus.DeliveryCompleted;
+        }
     }
 }
